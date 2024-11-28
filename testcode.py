dff9a5919d91f339184e270d32528bf9b0e87d22
lovefreeUnity <jo-ro3430@naver.com>
2024-11-28 16:02:13 +0900

    tru catch 제거

--- testcode.py
+++ testcode.py
@@ -16,38 +16,23 @@ options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 driver.get("http://localhost:8080")
 # [Test1]
-try:
-    title = WebDriverWait(driver=driver,timeout= 10).until(
-        EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/h1'))
-    )
-except TimeoutException:
-    print("[Test1] Title 요소를 찾는 데 시간이 초과되었습니다.")
-except Exception as e:
-    print(f"[Test1] 다른 예외 발생: {e}")
+title = WebDriverWait(driver=driver,timeout= 10).until(
+    EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/h1'))
+)
 
 if(title.text != "Wellcom to my app"):
     raise CustomException("title initalize error")
 
 # [Test2] 버튼 찾기
-try:
-    button = WebDriverWait(driver=driver,timeout= 10).until(
-        EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/button'))
-    )
-except TimeoutException:
-    print("[Test2] 요소를 찾는 데 시간이 초과되었습니다.")
-except Exception as e:
-    print(f"[Test2] 다른 예외 발생: {e}")
+button = WebDriverWait(driver=driver,timeout= 10).until(
+    EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/button'))
+)
 button.click()
 
 # [Test3] title 찾기
-try:
-    title = WebDriverWait(driver=driver,timeout= 10).until(
-        EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/h1'))
-    )
-except TimeoutException:
-    print("[Test3] 요소를 찾는 데 시간이 초과되었습니다.")
-except Exception as e:
-    print(f"[Test3] 다른 예외 발생: {e}")
+title = WebDriverWait(driver=driver,timeout= 10).until(
+    EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/h1'))
+)
 
 if(title.text != "Hellow Love Free Unity"):
     raise CustomException("[Test3] title changed error")
